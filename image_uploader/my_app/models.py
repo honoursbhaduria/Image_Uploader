96ff1fe327cc611e1f58dc3c27a874d1f565efc1
honours <honoursbhadauria@gmail.com>
2025-05-28 10:37:34 +0530

    Initialize Django project structure with basic settings, ASGI, WSGI, and URL configurations

---
+++ image_uploader/my_app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Image(models.Model):
+    photo  models.models.ImageField(_(""), upload_to="myimage", height_field=None, width_field=None, max_length=None)
+    
+
+
+
