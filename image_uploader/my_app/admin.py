6b0c767ca796d67b6e183fc19b271be8e48efa40
honours <honoursbhadauria@gmail.com>
2025-05-30 23:22:17 +0530

    Refactor code structure for improved readability and maintainability

--- image_uploader/my_app/admin.py
+++ image_uploader/my_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Image
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'photo', 'date']
+    search_fields = ['id',]
 # Register your models here.
+
--- image_uploader/my_app/models.py
+++ image_uploader/my_app/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Image(models.Model):
-    photo  models.models.ImageField(_(""), upload_to="myimage", height_field=None, width_field=None, max_length=None)
+    photo  = models.ImageField(_(""), upload_to="myimage", height_field=None, width_field=None, max_length=None)
+    date = models.DateTimeField( auto_now_add=True) 
     
 
 
